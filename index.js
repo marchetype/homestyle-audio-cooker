e450c4721a3c78778ccf166da8994c6c5e83fc43
marchetype <114883447+marchetype@users.noreply.github.com>
2023-03-31 11:42:20 -0400

    installed handlebars for modular views. restructuring in process for repository

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 //requiring electron in order to initialize the GUI
 const { app, BrowserWindow } = require('electron');
+//handlebars imports
+const path = require('path');
+const url = require('url');
+const handlebars = require('handlebars');
+//define a function that renders a view using Handlebars
+function renderView(viewName, data) {
+    //generate the file path of the view file
+    const viewPath = path.join(__dirname, 'views', `${viewName}.hbs`);
+    //read the contents of the view file
+    const viewContent = fs.readFileSync(viewPath, 'utf8');
+    //compile the view content into a function using Handlebars
+    const template = handlebars.compile(viewContent);
+    //use the compiled function to generate HTML code using the provided data
+    const html = template(data);
+    //load the generated HTML into the main Electron window
+    mainWindow.loadURL(`data:text/html;charset=UTF-8,${encodeURIComponent(html)}`);
+  }
 //creating a new window, with height/width dimensions listed.
 function createWindow () {
     const win = new BrowserWindow({
