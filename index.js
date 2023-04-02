a0452d2ce31b37124d508f7c2f768bfc909c8635
marchetype <114883447+marchetype@users.noreply.github.com>
2023-04-02 18:05:16 -0400

    styling will be paused for the moment. I need to work on functionality code. JavaScript time baybee

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
+const electron = require('electron');
 //requiring electron in order to initialize the GUI
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow } = electron;
 //handlebars imports
 const path = require('path');
 const url = require('url');
 const handlebars = require('handlebars');
 const fs = require('fs');
+
+let mainWindow;
 //define a function that renders a view using Handlebars
 function renderView(viewName, data) {
     //generate the file path of the view file
-    const viewPath = path.join(__dirname, 'views', `${viewName}.hbs`);
+    const viewPath = path.join(__dirname, 'views/layout', `${viewName}.hbs`);
+    console.log(viewPath);
     //read the contents of the view file
     const viewContent = fs.readFileSync(viewPath, 'utf8');
     //compile the view content into a function using Handlebars
@@ -20,15 +24,24 @@ function renderView(viewName, data) {
   }
 //creating a new window, with height/width dimensions listed.
 function createWindow () {
-    const win = new BrowserWindow({
+    mainWindow = new BrowserWindow({
         width: 800,
         height: 600,
         webPreferences: {
             nodeIntegration: true
         }
     });
+    mainWindow.webContents.on('did-finish-load', function() {
+        mainWindow.webContents.insertCSS(fs.readFileSync(path.join(__dirname, 'assets/css/style.css'), 'utf8'));
+      });
+    renderView('main', { body: 
+       `<h1></h1>
+        <section class="input_area">
+        <button
+        </section>`
+    }, mainWindow);
     //HAVING TROUBLE WITH VIEW HERE
-    renderView('main', { body: 'body content'})
+    //win.renderView('main', { body: 'body content'});
 }
 //if there are no browser windows open, open one
 app.whenReady().then(() => {
